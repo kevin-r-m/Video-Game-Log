d747deef98c94b68197afffafe84a32092b62df1
kevin-m <kmclaughlinbtb@gmail.com>
2021-06-12 19:26:56 -0500

    configured view engine

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 //Imports
 const express = require('express');
 const VGLogController = require('./controllers/vglog')
+const ejsLayouts = require('express-ejs-layouts')
 
 //Configurations
 const app = express();
 app.use('/games', VGLogController);
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
 
 //Ports
 app.listen(4000, () => console.log('Spinning on port: 4000'))
