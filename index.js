d4c0c32d9df1bbd8e8beb04b78ea2ddbe4b3c723
kevin-m <kmclaughlinbtb@gmail.com>
2021-06-20 15:56:29 -0500

    configurations for styling and js files

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 //Imports
 const express = require('express');
 const app = express();
-const methodOverride = require('method-override');
 const VGLogController = require('./controllers/vglog')
-const ejsLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override');
 
 //Configurations
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/games', VGLogController);
-app.use(methodOverride('_method'));
 app.set('view engine', 'ejs')
-app.use(ejsLayouts)
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + "/public"))
 
 //Ports
 app.listen(4000, () => console.log('Spinning on port: 4000'))
