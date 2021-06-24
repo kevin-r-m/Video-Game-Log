0a9a6533d40166875d950aed4e3523464bee9cf6
kevin-m <kmclaughlinbtb@gmail.com>
2021-06-23 21:05:37 -0500

    added js for search functionality, adjusted css to better allow searching

---
+++ public/JS/script.js
@@ -0,0 +1,21 @@
+console.log('this is connected')
+
+
+//Search Function
+const searchGames = () => {
+    let input, filter, div, name, a, txtValue
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    div = document.getElementsByClassName('gamePrev')
+    name = document.getElementsByClassName('titleThumb')
+
+    for (i = 0; i < name.length; i++){
+        a = name[i]
+        txtValue = a.textContent || a.innerText;
+        if(txtValue.toUpperCase().indexOf(filter) > -1){
+            div[i].style.display = ""
+        }else{
+            div[i].style.display='none'
+        }
+    }
+}
