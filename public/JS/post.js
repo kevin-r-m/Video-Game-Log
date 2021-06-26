529a1dd5e663a8daba4ba4b38ad1af0b1a3d1339
kevin-m <kmclaughlinbtb@gmail.com>
2021-06-26 17:00:18 -0500

    added function to count number of games on screen and update DOM

--- public/JS/post.js
+++ public/JS/post.js
@@ -1,5 +1,4 @@
 //Calculating averave review score
-
 const scores = document.querySelectorAll('.score')
 const userAverage = document.querySelector('.userAverage')
 
@@ -17,50 +16,46 @@ if (scores.length > 0){
     userAverage.innerHTML = scoreAverage
 }
 
+///////////////////////////////////////////////////////
 
 //Count number of games in DOM
 const games = document.querySelectorAll('.gamePrev')
 const gameNum = document.querySelector('.gameNum')
 
 var gameCount = 0
-if (games.length > 0){
-    games.forEach((game) =>{
-        if(game.style.display === 'none'){
-            return
-        }else{
-            gameCount += 1
-        }
-    })
-    const shownGames = games.length
 
-    console.log(gameCount)
+const countGames = () => {
+    var shownGames = 0
+    const updateNum = () =>{gameNum.innerHTML = shownGames}
+    var games = document.querySelectorAll('.gamePrev')
+        games.forEach((game) =>{
+            if(game.style.display === 'none'){
+            }else{shownGames += 1}
+            updateNum()
+        })
+    }
 
-    gameNum.innerHTML = shownGames
-}
+///////////////////////////////////////////////////////
 
 //Adding in Delete Modal
-// Get the modal
 const modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("removeBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementById("close");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+if(btn){
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
     modal.style.display = "none";
-  }
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
